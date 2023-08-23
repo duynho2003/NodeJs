6ae88f82ca428613610cedbf95badf4f97956bad
Tommy Nguyen <khenchaynguyen@gmail.com>
2023-08-23 16:36:34 +0700

    up

--- New/index.js
+++ New/index.js
@@ -11,14 +11,44 @@ app.set("views","./views"); //set views
 app.set("view engine", "ejs"); //set duoi mo rong ejs
 app.listen(port);
 
+//ket noi database
+let mongoose=require("mongoose");
+mongoose.connect('mongodb://127.0.0.1:27017/mahoa',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+let db=mongoose.connection;
+db.on("error",()=>{
+    console.log("not ok nhe");
+});
+db.once("open",()=>{
+    console.log("ket noi thanh cong");
+});
 
 //router
 app.get("/",function(req,res){
     res.render("home");
 });
+
+
 app.get("/dang-nhap",function(req,res){
     res.render("dangnhap")
 });
+app.post("/form-dang-nhap",function(req,res){
+    console.log(req.body.email);
+    console.log(req.body.password);
+
+    res.redirect("/")
+});
+
 app.get("/dang-ky",function(req,res){
+    
     res.render("dangky")
+});
+app.post("/form-dang-ky",function(req,res){
+    console.log(req.body.username);
+    console.log(req.body.email);
+    console.log(req.body.password);
+
+    res.redirect("/dang-nhap");
 });
