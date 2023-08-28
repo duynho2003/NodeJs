105b73e76f1a4d40b41d0988bbe0f3a42bbd3800
Tommy Nguyen <khenchaynguyen@gmail.com>
2023-08-28 18:47:13 +0700

    up

--- New/router/router.js
+++ New/router/router.js
@@ -1,7 +1,7 @@
 const express=require("express");
 const router=express.Router();
 const User=require("../model/users");
-const crypto=require("crypto")
+const crypto=require("crypto");
 
 router.get("/",function(req,res){
     res.render("home",{
@@ -35,8 +35,8 @@ router.get("/user-sua/:id",function(req,res){
     const id=req.params.id;
     User.findById(id).then(datauser=>{
         res.render("user/sua",{
-            title:"Sửa thành viên",
-            tv: datauser,
+            title:"sửa thành viên",
+            tv:datauser,
         });
     });
 });
@@ -45,12 +45,12 @@ router.post("/user-sua-form/:id",function(req,res){
     User.findByIdAndUpdate(id,{
         username: req.body.username,
         password: req.body.password,
-        email: req.body.email,
+        email:req.body.email,
     }).then(()=>{
-        res.redirect("/user")
+        res.redirect("/user");
     });
 });
-router.get("/user-xoa/:id",function(res,req){
+router.get("/user-xoa/:id",function(req,res){
     const id=req.params.id;
     User.findByIdAndDelete(id).then(()=>{
         res.redirect("/user");
@@ -60,20 +60,21 @@ router.get("/user-xoa/:id",function(res,req){
 
 //thuat toan
 router.get("/thuat-toan-des",function(req,res){
-    
+
     res.render("thuattoan/form",{
         mahoa:"",
     });
 });
 router.post("/thuat-toan-des",function(req,res){
-    let key=Buffer.from("hi54u89u593u590u940rtu5joigj3131","base64");
+    let key=Buffer.from("hsay21w1e621ty1uss1w62sh12sxsasa","base64");
     var mahoa=crypto.createCipheriv("des-ede3",key,null);
     const mahoadulieu=mahoa.update(req.body.vanban,"utf-8","base64")+mahoa.final("base64");
     console.log("Chuỗi mã hóa: "+mahoadulieu);
-    
-    // var giaima=crypto.createDecipheriv("des-ede3", key, null);
+
+    // var giaima=crypto.createDecipheriv("des-ede3",key,null);
     // var giaimadulieu=giaima.update(mahoadulieu,"base64","utf-8")+giaima.final("utf-8");
-    // console.log("Chuỗi giải mã là: " +giaimadulieu);
+    // console.log("Chuỗi giải mã la: "+giaimadulieu);
+
 
 
     res.render("thuattoan/form",{
