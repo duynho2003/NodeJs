0967dbb78fe34f2fa8bcaf685991c09c0f4a9ce4
Tommy Nguyen <khenchaynguyen@gmail.com>
2023-08-30 16:23:34 +0700

    up

--- code lession 2/server.js
+++ code lession 2/server.js
@@ -1,9 +1,18 @@
+//tao server don gian voi framework express
 const express=require("express");
 const app=express();
 const port=3000;
 
 app.get("/",function(req,res){
-    res.send("xin chao");
+    res.send("main page");
+});
+app.get("/hello/:type/:q",function(req,res){
+    const bien1=req.params.type;
+    const bien2=req.params.q;
+    res.send("get hello "+bien1+"--"+bien2);
+});
+app.post("/hello",function(req,res){
+    res.send("post hello");
 });
 
 app.listen(port,function(){
