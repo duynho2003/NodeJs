81ed6d692ca9941222dc9c39bee8b7684b7227c7
Tommy Nguyen <khenchaynguyen@gmail.com>
2023-09-08 16:21:50 +0700

    up

--- lesson5_6/mongo.js
+++ lesson5_6/mongo.js
@@ -2,14 +2,14 @@ var mongoose=require("mongoose");
 mongoose.connect('mongodb://127.0.0.1:27017/mongo');
 
 var db=mongoose.connection;
-db.on("error",()=>{
+db.on('error',()=>{
     console.log("error");
 });
-db.once("open",()=>{
+db.once('open',()=>{
     console.log("ket noi ok");
-});
+})
 
-//defind schema
+//define schema
 var BooksChema=mongoose.Schema({
     name:String,
     price:Number,
@@ -20,19 +20,19 @@ var Book=mongoose.model("books",BooksChema);
 //insert 1 record
 // var book1=new Book({
 //     name:'nguoi lai do song da 1',
-//     price:20000,
-//     quantity:1,
+//     price:2000000,
+//     quantity:1
 // });
-// book1.save()
+// book1.save();
 //liet ke list book
 Book.find().then((data)=>{
-    console.log(data);
+    console.log("hien thi danh sach: "+data);
 });
 //tim 1 record theo id
-Book.find({_id:'64fae5ed6d41d4cc95b0c367'}).then((data)=>{
-    console.log("Tim record theo id: "+data)
-});
-//xoa 1 record theo id
-// Book.deleteOne({_id:'64fae6e2660ca8b76088ba1f'}).then((data)=>{
-//     console.log(data);
+// Book.find({_id:'64fae536b273800736f87a7b'}).then((data)=>{
+//     console.log("Tim record theo id: "+data);
 // });
+//xoa 1 record theo id
+Book.deleteOne({_id:'64fae536b273800736f87a7b'}).then((data)=>{
+    console.log(data);
+});
