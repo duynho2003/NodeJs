e1af53b1419f1d7ccfdd874614cc2712ea65c56e
Tommy Nguyen <khenchaynguyen@gmail.com>
2023-09-15 15:15:16 +0700

    up

--- S06ProductShop/routes/dist/products.dev.js
+++ S06ProductShop/routes/dist/products.dev.js
@@ -2,4 +2,148 @@
 
 var express = require('express');
 
+var multer = require('multer');
+
 var router = express.Router();
+
+var ProductModel = require('../models/product.model');
+
+var storage = multer.diskStorage({
+  destination: function destination(req, file, cb) {
+    cb(null, './public/images');
+  },
+  filename: function filename(req, file, cb) {
+    var allowMineTypes = ['image/png', 'image/jpeg'];
+
+    if (!allowMineTypes.includes(file.mimetype)) {
+      var errorMessage = 'Not supported minetype';
+      cb(errorMessage, null);
+    } else {
+      cb(null, "".concat(file.fieldname, "-").concat(Date.now(), ".jpg"));
+    }
+  }
+});
+var upload = multer({
+  storage: storage
+});
+/* GET home page. */
+
+router.get('/', function _callee(req, res, next) {
+  var products;
+  return regeneratorRuntime.async(function _callee$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+          _context.next = 2;
+          return regeneratorRuntime.awrap(ProductModel.find());
+
+        case 2:
+          products = _context.sent;
+          console.log(products);
+          res.render('products/index', {
+            title: 'List product',
+            products: products
+          });
+
+        case 5:
+        case "end":
+          return _context.stop();
+      }
+    }
+  });
+});
+/* GET search home page. */
+
+router.get('/search', function _callee2(req, res, next) {
+  var _req$query, min, max, products;
+
+  return regeneratorRuntime.async(function _callee2$(_context2) {
+    while (1) {
+      switch (_context2.prev = _context2.next) {
+        case 0:
+          _req$query = req.query, min = _req$query.min, max = _req$query.max;
+          console.log('min ${min} -- max ${max}');
+          _context2.next = 4;
+          return regeneratorRuntime.awrap(ProductModel.find());
+
+        case 4:
+          products = _context2.sent;
+          console.log(products);
+          res.render('products/index', {
+            title: 'List product',
+            products: products
+          });
+
+        case 7:
+        case "end":
+          return _context2.stop();
+      }
+    }
+  });
+});
+/* GET Create product. */
+
+router.get('/create', function (req, res, next) {
+  res.render('products/create', {
+    title: 'Create new product'
+  });
+});
+/* POST Create product. */
+
+router.post('/create', upload.single('image'), function _callee3(req, res, next) {
+  var errorMessage, newProduct;
+  return regeneratorRuntime.async(function _callee3$(_context3) {
+    while (1) {
+      switch (_context3.prev = _context3.next) {
+        case 0:
+          if (req.file) {
+            _context3.next = 3;
+            break;
+          }
+
+          errorMessage = "No file uploaded";
+          return _context3.abrupt("return", next(errorMessage));
+
+        case 3:
+          //Save Product
+          newProduct = new ProductModel({
+            name: req.body.name,
+            price: req.body.price,
+            image: req.file.filename
+          });
+          _context3.next = 6;
+          return regeneratorRuntime.awrap(newProduct.save());
+
+        case 6:
+          return _context3.abrupt("return", res.redirect('/products'));
+
+        case 7:
+        case "end":
+          return _context3.stop();
+      }
+    }
+  });
+});
+/* GET Delete page. */
+
+router.get('/delete/:id', function _callee4(req, res, next) {
+  var id;
+  return regeneratorRuntime.async(function _callee4$(_context4) {
+    while (1) {
+      switch (_context4.prev = _context4.next) {
+        case 0:
+          id = req.params.id;
+          _context4.next = 3;
+          return regeneratorRuntime.awrap(ProductModel.findByIdAndDelete(id));
+
+        case 3:
+          res.redirect("/products");
+
+        case 4:
+        case "end":
+          return _context4.stop();
+      }
+    }
+  });
+});
+module.exports = router;
---
+++ S06ProductShop/routes/products.js
@@ -0,0 +1,67 @@
+var express = require('express');
+var multer = require('multer');
+var router = express.Router();
+const ProductModel = require('../models/product.model');
+
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, './public/images')
+  },
+  filename: function(req, file, cb) {
+    const allowMineTypes = ['image/png', 'image/jpeg'];
+    if (!allowMineTypes.includes(file.mimetype)) {
+    const errorMessage = 'Not supported minetype';
+    cb(errorMessage, null);
+  } else {
+      cb(null, `${file.fieldname}-${Date.now()}.jpg`)
+    }
+  }
+})
+  
+  const upload = multer({storage: storage});
+
+/* GET home page. */
+router.get('/', async function(req, res, next) {
+  const products = await ProductModel.find();
+  console.log(products);
+  res.render('products/index', { title: 'List product', products: products});
+});
+
+/* GET search home page. */
+router.get('/search', async function(req, res, next) {
+  const { min, max } = req.query;
+  console.log('min ${min} -- max ${max}');
+  const products = await ProductModel.find();
+  console.log(products);
+  res.render('products/index', { title: 'List product', products: products});
+});
+
+/* GET Create product. */
+router.get('/create', function(req, res, next) {
+  res.render('products/create', { title: 'Create new product'});
+});
+
+/* POST Create product. */
+router.post('/create', upload.single('image'), async function (req, res, next) {
+  if (!req.file) {
+      const errorMessage = "No file uploaded";
+      return next(errorMessage);
+  }
+  //Save Product
+  let newProduct = new ProductModel({
+  name: req.body.name,
+  price: req.body.price,
+  image: req.file.filename,
+});
+await newProduct.save();
+return res.redirect('/products');
+});
+
+/* GET Delete page. */
+router.get('/delete/:id', async function(req, res, next){
+  const id = req.params.id;
+  await ProductModel.findByIdAndDelete(id);
+  res.redirect("/products");
+});
+
+module.exports = router;
