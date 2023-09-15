143ae0983a6270b555b0f25c01aa3f6875d017f4
Tommy Nguyen <khenchaynguyen@gmail.com>
2023-09-15 15:07:46 +0700

    up

--- S06ProductShop/models/dist/product.model.dev.js
+++ S06ProductShop/models/dist/product.model.dev.js
@@ -3,7 +3,7 @@
 var mongoose = require('mongoose'); // taoj schema product
 
 
-var Product = new mongoose.Schema({
+var Product = mongoose.Schema({
   name: {
     type: String,
     require: true
--- S06ProductShop/models/product.model.js
+++ S06ProductShop/models/product.model.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 // taoj schema product
-const Product = new mongoose.Schema({
-name:{
-type:String,
-require:true
-},
-price:Number,
-image:String
+const Product = mongoose.Schema({
+    name:{
+        type:String,
+        require:true    
+    },
+    price:Number,
+    image:String
 });
 
 module.exports = mongoose.model('Product', Product);
